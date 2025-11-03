4e9dac2c9211cd78a53948394bb2eb28c5b50c73
Philipp Edelmann <pedelmann@lanl.gov>
2025-11-03 08:24:06 -0700

    fix preprocessor logic
    PassiveReal is only defined if USE_CODIPACK is set.

--- components/omega/test/infra/LoggingTest.cpp
+++ components/omega/test/infra/LoggingTest.cpp
@@ -141,7 +141,7 @@ int testKokkosDataTypes(bool LogEnabled) {
 
    Kokkos::initialize();
    {
-#ifndef USE_CODIPACK
+#ifdef USE_CODIPACK
       Array1DPassiveReal test1d("test1dD", d1);
       Array2DPassiveReal test2d("test2dD", d1, d2);
 #else
@@ -193,7 +193,7 @@ int testKokkosDataTypes(bool LogEnabled) {
       if (LogEnabled && TestRun)
          RetVal += outputTestResult("Kokkos data type 2", "test2dD", Contains);
 
-#ifndef USE_CODIPACK
+#ifdef USE_CODIPACK
       HostArray1DPassiveReal test1dH("test1dH", d1);
       HostArray2DPassiveReal test2dH("test2dH", d1, d2);
 #else
